584b8e172889e51401b2446669782694d4ea54b9
Jérôme Carretero <cJ@zougloub.eu>
2016-05-20 18:41:52 -0400

    merger_py: reduce some debug prints priorities

--- plugins/python/merger.py
+++ plugins/python/merger.py
@@ -75,6 +75,8 @@ class Merger(Gst.Element):
 				except IndexError:
 					return
 
+			Gst.info("Buffers L % 3d R % 3d" % (len(self._bufs_l), len(self._bufs_r)))
+
 			buf_l0 = popleft_or_none(self._bufs_l)
 			buf_l1 = popleft_or_none(self._bufs_l)
 			buf_r0 = popleft_or_none(self._bufs_r)
@@ -108,37 +110,37 @@ class Merger(Gst.Element):
 				app_if(self._bufs_l, buf_l0)
 				return
 
-			Gst.info("Buffer L0 %s ts=%s sz=%s" \
+			Gst.debug("Buffer L0 %s ts=%s sz=%s" \
 			 % (buf_l0, Gst.TIME_ARGS(buf_l0.pts), buf_l0.get_size()))
-			Gst.info("Buffer L1 %s ts=%s sz=%s" \
+			Gst.debug("Buffer L1 %s ts=%s sz=%s" \
 			 % (buf_l1, Gst.TIME_ARGS(buf_l1.pts), buf_l1.get_size()))
-			Gst.info("Buffer R0 %s ts=%s sz=%s" \
+			Gst.debug("Buffer R0 %s ts=%s sz=%s" \
 			 % (buf_r0, Gst.TIME_ARGS(buf_r0.pts), buf_r0.get_size()))
-			Gst.info("Buffer R1 %s ts=%s sz=%s" \
+			Gst.debug("Buffer R1 %s ts=%s sz=%s" \
 			 % (buf_r1, Gst.TIME_ARGS(buf_r1.pts), buf_r1.get_size()))
 
 
 			if buf_l0.pts >= buf_r0.pts and buf_l0.pts <= buf_r0.pts + buf_r0.duration:
-				Gst.info("a")
+				Gst.debug("a")
 				buf_l = buf_l0
 				buf_r = buf_r0
 				app_if(self._bufs_l, buf_l1)
 				app_if(self._bufs_r, buf_r1)
 			elif buf_r0.pts >= buf_l0.pts and buf_r0.pts <= buf_l0.pts + buf_l0.duration:
-				Gst.info("b")
+				Gst.debug("b")
 				buf_l = buf_l0
 				buf_r = buf_r0
 				app_if(self._bufs_l, buf_l1)
 				app_if(self._bufs_r, buf_r1)
 			elif buf_r0.pts + buf_r0.duration < buf_l0.pts:
-				Gst.info("c")
+				Gst.debug("c")
 				app_if(self._bufs_l, buf_l1)
 				app_if(self._bufs_l, buf_l0)
 				app_if(self._bufs_r, buf_r1)
 				Gst.info("Next time for R")
 				continue
 			elif buf_l0.pts + buf_l0.duration < buf_r0.pts:
-				Gst.info("d")
+				Gst.debug("d")
 				app_if(self._bufs_r, buf_r1)
 				app_if(self._bufs_r, buf_r0)
 				app_if(self._bufs_l, buf_l1)
@@ -147,8 +149,10 @@ class Merger(Gst.Element):
 			else:
 				Gst.info("Not implemented!")
 
-			Gst.info("Pushing new buffer")
+			Gst.debug("Pushing new buffer for t=%s" % Gst.TIME_ARGS(buf_l.pts))
 
+			srch, srcw = self._srch, self._srcw
+			dsth, dstw = self._dsth, self._dstw
 
 			tz = list()
 
@@ -191,7 +195,7 @@ class Merger(Gst.Element):
 			self.srcvpad.push(buf_out)
 			tz.append(time.time())
 
-			Gst.info("t_alloc %.3f  t_cp %.3f t_push %.3f" \
+			Gst.debug("t_alloc %.3f  t_cp %.3f t_push %.3f" \
 			 % (tz[1] - tz[0], tz[2] - tz[1], tz[-1] - tz[-2]))
 		
 			buf_r.unmap(mir)
@@ -201,50 +205,56 @@ class Merger(Gst.Element):
 		"""
 		Chain function
 		"""
-		Gst.info("L Buffer L %s ts=%s sz=%s" \
+		Gst.debug("L Buffer L %s ts=%s sz=%s" \
 		 % (buf_l, Gst.TIME_ARGS(buf_l.pts), buf_l.get_size()))
 
 		self._bufs_l.append(buf_l)
 		self.process_video(parent)
 
-		res = Gst.FlowReturn.OK
+		res = Gst.FlowReturn.OK if len(self._bufs_l) < 1000 else Gst.FlowReturn.FLUSHING
 		return res
 
 	def _sinkr_chain(self, pad, parent, buf_r):
 		"""
 		Chain function
 		"""
-		Gst.info("R Buffer R %s ts=%s sz=%s" \
+		Gst.debug("R Buffer R %s ts=%s sz=%s" \
 		 % (buf_r, Gst.TIME_ARGS(buf_r.pts), buf_r.get_size()))
 
 		self._bufs_r.append(buf_r)
-		self.process_video(parent)
+		#self.process_video(parent)
 
-		res = Gst.FlowReturn.OK
+		res = Gst.FlowReturn.OK if len(self._bufs_r) < 1000 else Gst.FlowReturn.FLUSHING
 		return res
 
 	def setcaps_srcv(self, parent, caps):
 		othercaps = self.srcvpad.get_allowed_caps()
-		Gst.info("other caps %s" % othercaps)
+		Gst.debug("other caps %s" % othercaps)
+
+		self._srcw = caps[0]["width"]
+		self._srch = caps[0]["height"]
 
 		if 1:
 			outcaps = Gst.Caps('%s' % othercaps)
 
 			out_s = outcaps[0]
-			Gst.info("outcasp 0 %s" % (out_s))
-			Gst.info(" %s" % dir(out_s))
+			Gst.debug("outcasp 0 %s" % (out_s))
+			Gst.debug(" %s" % dir(out_s))
 
 			out_s.set_value("framerate", Gst.Fraction(30, 1))#caps[0]["framerate"])
 
-			Gst.info("out_s %s" % out_s.to_string())
+			Gst.debug("out_s %s" % out_s.to_string())
 
-			Gst.info(" dir %s" % dir(outcaps))
+			Gst.debug(" dir %s" % dir(outcaps))
 			#outcaps.set_structure(0, out_s)
 			outcaps = Gst.Caps.from_string(out_s.to_string())
 
 
-			Gst.info("pad is %s" % self.srcvpad.__class__)
-			Gst.info("pad can %s" % dir(self.srcvpad))
+			Gst.debug("pad is %s" % self.srcvpad.__class__)
+			Gst.debug("pad can %s" % dir(self.srcvpad))
+
+		self._dstw = outcaps[0]["width"]
+		self._dsth = outcaps[0]["height"]
 
 		res = self.srcvpad.push_event(Gst.Event.new_caps(outcaps))
 
@@ -255,25 +265,25 @@ class Merger(Gst.Element):
 		return res
 
 	def _srcv_event(self, pad, parent, event):
-		Gst.info("event %s" % event)
-		Gst.info("event type %s" % event.type)
+		Gst.debug("event %s" % event)
+		Gst.debug("event type %s" % event.type)
 		return self.sinklpad.push_event(event) and self.sinkrpad.push_event(event)
 
 	def _sinkl_event(self, pad, parent, event):
-		Gst.info("event %s" % event)
-		Gst.info("event type %s" % event.type)
+		Gst.debug("event %s" % event)
+		Gst.debug("event type %s" % event.type)
 		if event.type == Gst.EventType.CAPS:
 			caps = event.parse_caps()
-			Gst.info("event caps %s" % caps)
+			Gst.debug("event caps %s" % caps)
 			return self.setcaps_srcv(parent, caps)
 		return self.srcvpad.push_event(event)
 
 	def _sinkr_event(self, pad, parent, event):
-		Gst.info("event %s" % event)
-		Gst.info("event type %s" % event.type)
+		Gst.debug("event %s" % event)
+		Gst.debug("event type %s" % event.type)
 		if event.type == Gst.EventType.CAPS:
 			caps = event.parse_caps()
-			Gst.info("event caps %s" % caps)
+			Gst.debug("event caps %s" % caps)
 			return self.setcaps_srcv(parent, caps)
 
 		return self.srcvpad.push_event(event)
